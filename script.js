712ea556bc7047bfa98215a61eb1481eefcb0eab
Nambhadane <namratabhadane937@gmail.com>
2025-05-13 17:24:54 +0530

    Updated portfolio content

--- script.js
+++
@@ -1,6 +0,0 @@
-<script>
-    document.querySelector('.menu-btn').addEventListener('click', () => {
-        const menuContent = document.querySelector('.menu-content');
-        menuContent.style.display = menuContent.style.display === 'flex' ? 'none' : 'flex';
-    });
-</script>
